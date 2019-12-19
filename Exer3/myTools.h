c3258e5a7676b6719abdb7af7f308ad58c8bdbe4
belzbuu <raphaelmansueto@gmail.com>
2019-12-19 09:45:45 +0800

    Add files via upload

---
+++ Exer3/myTools.h
@@ -0,0 +1,9 @@
+
+
+void menu();
+void drawSBox(int x1,int y1, int x2, int y2);
+void gotoxy(int x, int y);
+void center(int row, char str[]);
+void outString(int row, int col, char str[]);
+void drawLBox(int x, int y, int h, int w);
+
